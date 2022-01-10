126a32c12a5d408b5896e554d68c22982d2ca695
Mohal1125 <rajwindersinghmohal@yahoo.com>
2022-01-10 15:54:26 +0100

    fertig

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const number = [
     -44, 8, 9, -12, 70.5, -1, -7, -909, -18, 45, 33, 1921 , 1989
 ];
 const negativenZahlen = [];
-
+const positivenZahlen = [];
 number.forEach (
     (zahlen) => {
     if (zahlen < 0) {
         negativenZahlen.push(zahlen);
+    }else {
+        positivenZahlen.push(zahlen);
     }
 
 }
 );
 console.log(negativenZahlen);
+console.log(positivenZahlen);
