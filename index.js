0417ca7c30a213c74f98697d2978a3c63292344d
Mazen <mazen.taha88@gmail.com>
2022-01-10 15:38:23 +0100

    My Lösung

---
+++ index.js
@@ -0,0 +1,15 @@
+const Zahlen = [-44, 8, 9, -12, 70.5, -1, -7, -909, -18, 45, 33, 1921, 1989];
+
+const negativenZahlen = [];
+
+
+
+Zahlen.forEach((Zahlen) => {
+        if(Zahlen < 0){
+            negativenZahlen.push(Zahlen);
+
+        }
+    }
+);
+
+console.log(negativenZahlen);
