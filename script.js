dc13ae422f9d49dd34def0c332d5cb624bbc6173
Vens <v.kolev@alfatrex.com>
2022-01-10 15:23:43 +0100

    vens

---
+++ script.js
@@ -0,0 +1,12 @@
+const zahlen = [-44, 8, 9, -12, 70.5, -1, -7, -909, -18, 45, 33, 1921, 1989];
+
+const minusZahlen = [];
+
+zahlen.forEach (
+    (zahlen) => {
+    if (zahlen < 0) {
+        minusZahlen.push(zahlen);
+    }
+}
+);
+console.log(minusZahlen);
