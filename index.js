4902bd9242c033f0a3d80cfd1664d149e9053490
slavablack <veaceslav.ciornii@magenta.de>
2022-01-10 15:33:20 +0100

    forEach

---
+++ index.js
@@ -0,0 +1,22 @@
+const originalArray = [-44, 8, 9, -12, 70.5, -1, -7, -909, -18, 45, 33, 1921, 1989];
+
+const negativeArray = [];
+const positiveArray = [];
+
+originalArray.forEach (
+    (paramForEach) => {
+      if (paramForEach < 0) {
+          negativeArray.push(paramForEach);
+        
+    
+        }
+        else{
+          (paramForEach > 0)
+          positiveArray.push(paramForEach);
+        }
+      
+    }
+)
+ console.log(originalArray);
+console.log(negativeArray);
+console.log(positiveArray);
