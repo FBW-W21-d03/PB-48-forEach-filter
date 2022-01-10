ebab4384cac76cadf8abd938971d914cdf8e72b8
sprotte <metatron1972@live.de>
2022-01-10 15:33:17 +0100

    sprotte

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+"use strict";
+/*
 const zahlen = [-44, 8, 9, -12, 70.5, -1, -7, -909, -18, 45, 33, 1921, 1989];
 const ungeradeZahlen = [];
 let summe = 0;
@@ -12,3 +14,21 @@ for (ungerade = 0; ungerade < 20; ungerade++) {
     console.log(ungerade + 1 + "gerade");
   }
 }
+*/
+
+const zahlen = [-44, 8, 9, -12, 70.5, -1, -7, -909, -18, 45, 33, 1921, 1989];
+
+console.log("Aufgabe", zahlen);
+
+const negativenZahlen = [];
+console.log("Aufgabe2", zahlen);
+
+zahlen.forEach((negative) => {
+  if (negative < 0) {
+    console.log("Aufgabe3", negative);
+    negativenZahlen.push(negative);
+  }
+});
+
+negativenZahlen.sort();
+console.log("Aufgabe 4 \n", negativenZahlen);
